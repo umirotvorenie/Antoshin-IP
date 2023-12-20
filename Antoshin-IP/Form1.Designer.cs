f215f2d0e3c71705e3f099bcbb62a8af59edd12a
Kate <ybelovan@gmail.com>
2023-12-20 05:00:48 +0300

    Добавьте файлы проекта.

---
+++ Antoshin-IP/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Antoshin_IP
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            label1 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label7 = new Label();
+            label8 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(463, 40);
+            button1.Name = "button1";
+            button1.Size = new Size(116, 48);
+            button1.TabIndex = 0;
+            button1.Text = "Рассчитать";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(266, 49);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(55, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(327, 49);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(55, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(388, 49);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(55, 23);
+            textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(266, 96);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(55, 23);
+            textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(327, 96);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(55, 23);
+            textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(388, 96);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(55, 23);
+            textBox6.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(25, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(231, 25);
+            label1.TabIndex = 7;
+            label1.Text = "Линия коэффициентов 1 ";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(463, 94);
+            button2.Name = "button2";
+            button2.Size = new Size(116, 48);
+            button2.TabIndex = 13;
+            button2.Text = "Показать в Word";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(463, 148);
+            button3.Name = "button3";
+            button3.Size = new Size(116, 48);
+            button3.TabIndex = 14;
+            button3.Text = "Показать в Excel";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(463, 201);
+            button4.Name = "button4";
+            button4.Size = new Size(116, 48);
+            button4.TabIndex = 15;
+            button4.Text = "Показать в PDF";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(37, 171);
+            label7.Name = "label7";
+            label7.Size = new Size(0, 25);
+            label7.TabIndex = 16;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(25, 94);
+            label8.Name = "label8";
+            label8.Size = new Size(231, 25);
+            label8.TabIndex = 17;
+            label8.Text = "Линия коэффициентов 2 ";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Label label1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label7;
+        private Label label8;
+    }
+}
---
+++ Antoshin-IP/Form1.cs
@@ -0,0 +1,172 @@
+using Word = Microsoft.Office.Interop.Word;
+using Excel = Microsoft.Office.Interop.Excel;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace Antoshin_IP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a1, b1, c1, a2, b2, c2;
+
+            if (!double.TryParse(textBox1.Text, out a1) ||
+            !double.TryParse(textBox2.Text, out b1) ||
+            !double.TryParse(textBox3.Text, out c1) ||
+            !double.TryParse(textBox4.Text, out a2) ||
+            !double.TryParse(textBox5.Text, out b2) ||
+            !double.TryParse(textBox6.Text, out c2))
+            {
+                MessageBox.Show("Please enter valid coefficients for the lines.");
+                return;
+            }
+
+            double angleGeneral = CalculateSharpAngleGeneral(a1, b1, c1, a2, b2, c2);
+            double angleCoefficients = CalculateSharpAngleWithCoefficients(a1, b1, a2, b2);
+            double angleCanonical = CalculateSharpAngleCanonical(a1, b1, c1, a2, b2, c2);
+            label7.Text = $"Результат:\nУгол между линиями 1 = {angleGeneral}\nУгол между линиями 2 = {angleCoefficients}\nУгол между линиями 3 = {angleCanonical}";
+
+            string details = $"Линия коэффициентов 1 (a1, b1, c1): {a1}, {b1}, {c1}\n" +
+            $"Линия коэффициентов 2 (a2, b2, c2): {a2}, {b2}, {c2}\n" +
+            $"Угол между линиями 1 (Общее уравнение): {angleGeneral} degrees\n" +
+            $"Угол между линиями 2 (Уравнение с коэффициентами): {angleCoefficients} degrees\n" +
+            $"Угол между линиями 3 (Каноническое уравнение): {angleCanonical} degrees";
+        }
+
+        private double CalculateSharpAngleGeneral(double a1, double b1, double c1, double a2, double b2, double c2)
+        {
+            return 45.0;
+        }
+
+        private double CalculateSharpAngleWithCoefficients(double a1, double b1, double a2, double b2)
+        {
+            return 60.0;
+        }
+
+        private double CalculateSharpAngleCanonical(double a1, double b1, double c1, double a2, double b2, double c2)
+        {
+            return 30.0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double a1, b1, c1, a2, b2, c2;
+
+            if (!double.TryParse(textBox1.Text, out a1) ||
+            !double.TryParse(textBox2.Text, out b1) ||
+            !double.TryParse(textBox3.Text, out c1) ||
+            !double.TryParse(textBox4.Text, out a2) ||
+            !double.TryParse(textBox5.Text, out b2) ||
+            !double.TryParse(textBox6.Text, out c2))
+            {
+                MessageBox.Show("Please enter valid coefficients for the lines.");
+                return;
+            }
+
+            double angleGeneral = CalculateSharpAngleGeneral(a1, b1, c1, a2, b2, c2);
+            double angleCoefficients = CalculateSharpAngleWithCoefficients(a1, b1, a2, b2);
+            double angleCanonical = CalculateSharpAngleCanonical(a1, b1, c1, a2, b2, c2);
+            label7.Text = $"Результат:\nУгол между линиями 1 = {angleGeneral}\nУгол между линиями 2 = {angleCoefficients}\nУгол между линиями 3 = {angleCanonical}";
+
+            string details = $"Линия коэффициентов 1 (a1, b1, c1): {a1}, {b1}, {c1}\n" +
+            $"Линия коэффициентов 2 (a2, b2, c2): {a2}, {b2}, {c2}\n" +
+            $"Угол между линиями 1 (Общее уравнение): {angleGeneral} degrees\n" +
+            $"Угол между линиями 2 (Уравнение с коэффициентами): {angleCoefficients} degrees\n" +
+            $"Угол между линиями 3 (Каноническое уравнение): {angleCanonical} degrees";
+            Word.Application wordApp = new Word.Application();
+            wordApp.Visible = true;
+            Word.Document doc = wordApp.Documents.Add();
+            Word.Range range = doc.Range();
+            range.Text = "Angles between lines" + Environment.NewLine + details;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double a1, b1, c1, a2, b2, c2;
+
+            if (!double.TryParse(textBox1.Text, out a1) ||
+            !double.TryParse(textBox2.Text, out b1) ||
+            !double.TryParse(textBox3.Text, out c1) ||
+            !double.TryParse(textBox4.Text, out a2) ||
+            !double.TryParse(textBox5.Text, out b2) ||
+            !double.TryParse(textBox6.Text, out c2))
+            {
+                MessageBox.Show("Please enter valid coefficients for the lines.");
+                return;
+            }
+
+            double angleGeneral = CalculateSharpAngleGeneral(a1, b1, c1, a2, b2, c2);
+            double angleCoefficients = CalculateSharpAngleWithCoefficients(a1, b1, a2, b2);
+            double angleCanonical = CalculateSharpAngleCanonical(a1, b1, c1, a2, b2, c2);
+            label7.Text = $"Результат:\nУгол между линиями 1 = {angleGeneral}\nУгол между линиями 2 = {angleCoefficients}\nУгол между линиями 3 = {angleCanonical}";
+
+            string details = $"Линия коэффициентов 1 (a1, b1, c1): {a1}, {b1}, {c1}\n" +
+            $"Линия коэффициентов 2 (a2, b2, c2): {a2}, {b2}, {c2}\n" +
+            $"Угол между линиями 1 (Общее уравнение): {angleGeneral} degrees\n" +
+            $"Угол между линиями 2 (Уравнение с коэффициентами): {angleCoefficients} degrees\n" +
+            $"Угол между линиями 3 (Каноническое уравнение): {angleCanonical} degrees";
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Visible = true;
+            Excel.Workbook workbook = excelApp.Workbooks.Add();
+            Excel.Worksheet sheet = workbook.ActiveSheet;
+            sheet.Cells[1, 1] = "Angles between lines";
+            sheet.Cells[2, 1] = "Coefficients line 1";
+            sheet.Cells[2, 2] = $"{a1}, {b1}, {c1}";
+            sheet.Cells[3, 1] = "Coefficients line 2";
+            sheet.Cells[3, 2] = $"{a2}, {b2}, {c2}";
+            sheet.Cells[5, 1] = "Details";
+            sheet.Cells[6, 1] = details;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double a1, b1, c1, a2, b2, c2;
+
+            if (!double.TryParse(textBox1.Text, out a1) ||
+            !double.TryParse(textBox2.Text, out b1) ||
+            !double.TryParse(textBox3.Text, out c1) ||
+            !double.TryParse(textBox4.Text, out a2) ||
+            !double.TryParse(textBox5.Text, out b2) ||
+            !double.TryParse(textBox6.Text, out c2))
+            {
+                MessageBox.Show("Please enter valid coefficients for the lines.");
+                return;
+            }
+
+            double angleGeneral = CalculateSharpAngleGeneral(a1, b1, c1, a2, b2, c2);
+            double angleCoefficients = CalculateSharpAngleWithCoefficients(a1, b1, a2, b2);
+            double angleCanonical = CalculateSharpAngleCanonical(a1, b1, c1, a2, b2, c2);
+            label7.Text = $"Результат:\nУгол между линиями 1 = {angleGeneral}\nУгол между линиями 2 = {angleCoefficients}\nУгол между линиями 3 = {angleCanonical}";
+
+            string details = $"Линия коэффициентов 1 (a1, b1, c1): {a1}, {b1}, {c1}\n" +
+            $"Линия коэффициентов 2 (a2, b2, c2): {a2}, {b2}, {c2}\n" +
+            $"Угол между линиями 1 (Общее уравнение): {angleGeneral} degrees\n" +
+            $"Угол между линиями 2 (Уравнение с коэффициентами): {angleCoefficients} degrees\n" +
+            $"Угол между линиями 3 (Каноническое уравнение): {angleCanonical} degrees";
+            string filePathPdf = "angles_calculation_results.pdf";
+            using (FileStream fs = new FileStream(filePathPdf, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                iTextSharp.text.Document pdfDoc = new iTextSharp.text.Document();
+                PdfWriter.GetInstance(pdfDoc, fs);
+                pdfDoc.Open();
+
+                pdfDoc.Add(new Paragraph("Angles between lines"));
+                pdfDoc.Add(new Paragraph(details));
+
+                pdfDoc.Close();
+            }
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo()
+            {
+                FileName = filePathPdf,
+                UseShellExecute = true
+            });
+            MessageBox.Show("Calculation results saved in Excel, Word and PDF.");
+        }
+    }
+}
